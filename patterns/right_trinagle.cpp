5660f30193f6957cb3721d5e32779aa4bbf15b66
DhruboDevsharma <devsharmadhrubo@gmail.com>
2026-05-07 01:30:00 +0530

    code with space

--- patterns/right_trinagle.cpp
+++ patterns/right_trinagle.cpp
@@ -6,14 +6,13 @@ int main(){
     cin >> n;
 
     for(int i = 1; i <= n; i++){              // row control
-        // প্রথমে space প্রিন্ট করবো
-        for(int j = 1; j <= n - i; j++){
-            cout << "  ";                     // দুইটা space
-        }
-        // তারপর star প্রিন্ট করবো
-        for(int k = 1; k <= i; k++){
-            cout << "* ";
-        }
+      for(int j =1; j<= n-i; j++){
+        cout <<"  ";
+
+      }
+      for(int k =1; k<=i; k++){
+        cout <<"* ";
+      }
         cout << endl;
     }
     return 0;
