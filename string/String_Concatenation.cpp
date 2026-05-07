24e75372a491c98c9a3a9bcb56ec9aac6b4e49f3
DhruboDevsharma <devsharmadhrubo@gmail.com>
2026-05-08 01:04:31 +0530

    Writing the command line in concatenation file

--- string/String_Concatenation.cpp
+++ string/String_Concatenation.cpp
@@ -5,7 +5,12 @@ int main(){
     string First = "Dhrubo";
     string second = "Dev";
 
-    string full= First + second;
+    string full= First +" "+ second;
 
     cout << full;
+
+    //here + is work as concatenation. it also work as operator.
+    //+ called as concatenation in string and add in integer o numerical value.
+    //string and numerical number can't be concatenation
+
 }
