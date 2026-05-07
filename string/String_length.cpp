2ce19fdf7d4d702ac0d722347c2c605c03282dc4
DhruboDevsharma <devsharmadhrubo@gmail.com>
2026-05-08 01:17:00 +0530

    add size() function in length file

--- string/String_length.cpp
+++ string/String_length.cpp
@@ -5,6 +5,11 @@ int main(){
     string First = "Dhrubo";
     
 
-    cout << First.length();
+    cout << First.length()<< endl;
     
+
+    // another fuunction is used for find the size of string is size() function
+    string txt ="DEV";
+    cout <<txt.size();
+    return 0;
 }
