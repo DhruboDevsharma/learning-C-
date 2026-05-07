9e98a46886132a23e5ed9d50d5d72690670980cf
DhruboDevsharma <devsharmadhrubo@gmail.com>
2026-05-08 01:39:12 +0530

    Add code to getline() to get a full line as a input in c++

--- string/String_Line.cpp
+++ string/String_Line.cpp
@@ -1,13 +1,17 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 int main(){
     string First ;
+    string line ;
     cin>> First;
    
 
-    cout << First;
+    cout << First<<endl;
 
     // C++ input string as usual but i can input a full line using getline() function.
-    
+    getline(cin>>ws, line);
+
+    cout << line;
 }
