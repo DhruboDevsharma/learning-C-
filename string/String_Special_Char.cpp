fecddf07adc399d331ee52b281aa5d84b4c603b9
DhruboDevsharma <devsharmadhrubo@gmail.com>
2026-05-08 01:26:58 +0530

    Add special charecter access code.

---
+++ string/String_Special_Char.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string First = "Dhr\\u/bo";
+    
+    // c++ suport all special directly only \ and " can't directly access so we use \ first then \and ". like \\ and \".
+    
+
+    cout << First;
+}
