7029d45ce5e68c68642bdcdae24fe97725ab4710
DhruboDevsharma <devsharmadhrubo@gmail.com>
2026-05-08 01:30:03 +0530

    INput string as a line.

---
+++ string/String_Line.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string First ;
+    cin>> First;
+   
+
+    cout << First;
+
+    // C++ input string as usual but i can input a full line using getline() function.
+    
+}
