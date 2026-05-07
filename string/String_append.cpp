c6f3dff293eb8e3ecd1f7e052af966a0e2ab93f3
DhruboDevsharma <devsharmadhrubo@gmail.com>
2026-05-08 01:11:20 +0530

    ADD a append() function file

---
+++ string/String_append.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string First = "Dhrubo";
+    string second = "DevSharma";
+
+    string full= First.append(" "+second);
+
+    cout << full;
+}
