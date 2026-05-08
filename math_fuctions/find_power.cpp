3b04c4d81a8ce54a912d5f70f5b141f1c5ee1f8d
DhruboDevsharma <devsharmadhrubo@gmail.com>
2026-05-09 00:56:37 +0530

    using pow() function

---
+++ math_fuctions/find_power.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+    int a =5;
+    int b =2;
+    
+
+    
+    cout << pow(a,b);
+    // need to call <cmath> library to use tthis function.
+    return 0;
+}
