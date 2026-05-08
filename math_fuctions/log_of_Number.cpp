9bb32a35c1d2234e836fd7f680d7949728397399
DhruboDevsharma <devsharmadhrubo@gmail.com>
2026-05-09 00:55:02 +0530

    log of a number

---
+++ math_fuctions/log_of_Number.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+    int a =5;
+    
+
+    
+    cout << log(a);
+    // need to call <cmath> library to use tthis function.
+    return 0;
+}
