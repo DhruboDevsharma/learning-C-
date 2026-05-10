92d087a854386b08335d7414099f3c12889eb942
DhruboDevsharma <devsharmadhrubo@gmail.com>
2026-05-11 00:21:40 +0530

    add a new file using loop

---
+++ LOOPS/print_number.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    cout << "Enter the number want to print" << endl;
+
+    int n;
+    cin >> n; 
+
+    for(int i =1; i<=n; i++){
+        cout << i << endl;
+    }
+}   
