4416e54a8ba888507a5e7d1b71385083f9eca299
DhruboDevsharma <devsharmadhrubo@gmail.com>
2026-05-10 23:40:29 +0530

    testing if eles condition

---
+++ conditions/ODDorEVEN_number.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int a;
+    cin>> a ;
+
+    if(a%2 == 0){
+        cout << a << " is ever number.";
+
+    }
+    else cout << a << " is odd number";
+  
+
+
+
+    
+
+    return 0;
+
+}
