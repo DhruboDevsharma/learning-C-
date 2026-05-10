633b2ca1c683678941c032d5ffae3f41a68f7f53
DhruboDevsharma <devsharmadhrubo@gmail.com>
2026-05-10 23:44:11 +0530

    finder the bigger number

---
+++ conditions/max_number.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+ using namespace std;
+
+ int main(){
+
+    int a, b;
+
+    cin >> a >> b ;
+
+    if (a>b){
+        cout << a << " is a bigger number";
+    }
+    else {
+        cout << b << " is a Bigger number";
+    }
+
+ }
