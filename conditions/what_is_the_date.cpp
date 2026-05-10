a227651d576c5b2a6c8a1cffdfa9ff027843b135
DhruboDevsharma <devsharmadhrubo@gmail.com>
2026-05-11 00:14:04 +0530

    Switch case

---
+++ conditions/what_is_the_date.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+ int main(){
+
+    int day;
+    
+    cin>> day;
+
+    switch(day){
+        case 1: cout <<"Monday";
+        break;
+        case 2: cout << "Tuesday";
+        break;
+        case 3: cout << "Wednessday";
+        break;
+        case 4: cout << "Thusday";
+        break;
+        case 5 : cout << "Friday";
+        break;
+        case 6 : cout <<"Saturday";
+        break;
+        case 7: cout << "Sunday";
+        break;
+        default: cout << "INVALID NUMBER";
+        
+    }
+ }
