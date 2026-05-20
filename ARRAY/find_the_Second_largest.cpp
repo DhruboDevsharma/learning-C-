781acb1eabe7dbd12e8bec1c31caa979c34b5104
DhruboDevsharma <devsharmadhrubo@gmail.com>
2026-05-20 13:06:43 +0530

    Finding the second largest number in array

--- ARRAY/find_the_Second_largest.cpp
+++ ARRAY/find_the_Second_largest.cpp
@@ -1,35 +1,34 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-
-    cout << "ENter the arr number"<< endl;
-    int n ;
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
     cin >> n;
 
     int arr[n];
-    for(int i=0; i<n; i++){
-        cin>> arr[i];
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
 
-    int max = arr[0];
+    int largest = arr[0];
+    int second = -1;  // assuming positive numbers; adjust if negatives possible
 
-    for(int i=0; i<n; i++){
-        if(max< arr[i]){
-            max = arr[i];
-        }    
-    }
-    int sec= 0;
-    for(int i=0;i<n; i++){
-        if(sec<max){
-            sec = sec;
-
-        if( sec>arr[i]){
-            sec=arr[i];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > largest) {
+            second = largest;   // old largest becomes second
+            largest = arr[i];   // update largest
         }
-    }
-}
-   
-    cout <<"Second largest "<< sec;
+        else if (arr[i] > second && arr[i] != largest) {
+            second = arr[i];    // update second largest
+        }
 
+        
+    }
+
+    cout << "Largest = " << largest << endl;
+    cout << "Second Largest = " << second << endl;
+
+    return 0;
 }
