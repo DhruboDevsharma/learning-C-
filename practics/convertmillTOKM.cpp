631c99dbe295317825d35d2f0494356d00597c71
DhruboDevsharma <devsharmadhrubo@gmail.com>
2026-06-06 22:46:03 +0530

    Convert mile to km

---
+++ practics/convertmillTOKM.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int mile;
+    cin >> mile;
+
+    float km;
+    km = 1.609 * mile;
+
+    cout << "Mile into KM :" << km << endl;
+
+    return 0;
+}
