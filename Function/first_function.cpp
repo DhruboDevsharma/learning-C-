8dcb7b8d4cf1dfbbb86def0752809b408ec50088
DhruboDevsharma <devsharmadhrubo@gmail.com>
2026-06-08 22:40:30 +0530

    Add comment line

--- Function/first_function.cpp
+++ Function/first_function.cpp
@@ -5,6 +5,9 @@ using namespace std;
 // functions are used to modularise
 // functions are uset to use same code multiple of time
 // functions are used to increse code readability
+// functions can be void function . means this function do not return anything
+// functions can be return function. Means this function return any type of data
+// functions can have perameter or non perameter
 
 int main(){
 
