48dcfdda13ea6b4cf13e1d66af969724239e676e
DhruboDevsharma <devsharmadhrubo@gmail.com>
2026-06-08 22:45:52 +0530

    Writing first fuunction with parameter

---
+++ Function/void_Type_Parameter.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void My_name(string name){
+    cout <<"I am "<< name << endl;
+}
+
+int main(){
+
+    string name;
+
+    cin >> name;
+
+    My_name(name);  
+}
