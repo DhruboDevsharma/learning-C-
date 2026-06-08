aa0476600bb9a849e66a60c758a69b4283db1f09
DhruboDevsharma <devsharmadhrubo@gmail.com>
2026-06-08 22:30:59 +0530

    First function code

---
+++ Function/first_function.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// functions are set of code which performs something specfic task
+// functions are used to modularise
+// functions are uset to use same code multiple of time
+// functions are used to increse code readability
+
+int main(){
+
+}
