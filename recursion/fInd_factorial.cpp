1439b7befb791511a3c484459771168a173ba3e3
DhruboDevsharma <devsharmadhrubo@gmail.com>
2026-06-10 10:58:55 +0530

    Finding factorial of n number using recursion

---
+++ recursion/fInd_factorial.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int fact(int n){
+    if(n == 1 || n ==0){
+        return 1;
+    }
+    else{
+        return n*fact(n-1);
+    }
+
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int fac = fact(n);
+
+    cout << "Factorial : "  <<  fac<< endl;
+    return 0;
+}
