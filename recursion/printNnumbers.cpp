950ea2c03b33139c6f0d0bcd513cc016d8306535
DhruboDevsharma <dhrubodevsharma@gmail.com>
2026-06-13 13:53:35 +0530

    Modified

--- recursion/printNnumbers.cpp
+++ recursion/printNnumbers.cpp
@@ -2,16 +2,23 @@
 using namespace std;
 
 int Print(int n){
-    if(n== 0) 
-        return 0;
-    else{
+    if(n == 0){
+    return 0;
+}
+    else {
+        cout << n << endl;
         return Print(n-1);
     }
 }
 
 int main(){
     int n;
-    cin>>n;
-    Print(n);
-    return 0;
+
+    cin >> n;
+
+    int v= Print(n);
+
+    cout << v << " ";
+
+    
 }
