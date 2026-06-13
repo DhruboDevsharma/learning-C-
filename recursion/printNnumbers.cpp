caeb13eb4cf0e4d856a681a053a67b93bbc5a346
DhruboDevsharma <dhrubodevsharma@gmail.com>
2026-06-13 13:47:57 +0530

    NEw code

---
+++ recursion/printNnumbers.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int Print(int n){
+    if(n== 0) 
+        return 0;
+    else{
+        return Print(n-1);
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    Print(n);
+    return 0;
+}
